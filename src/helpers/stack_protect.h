29f47b4674392b71e571f9efc00d94f66e235448
Yehor Popovych <yehor@tesseract.one>
2022-05-05 18:41:31 +0300

    better stack protection

--- src/helpers/stack_protect.h
+++ src/helpers/stack_protect.h
@@ -1,11 +1,22 @@
 #pragma once
 
+#include <stdint.h>
+#include <os.h>
+#include "../sw.h"
+
 // This symbol is defined by the link script to be at the start of the stack
 // area.
 extern unsigned long _stack;
 
+#define STACK_CANARY (*((volatile uint32_t*) &_stack))
+
 // Init value for stack overflow checking
-void init_canary(void);
+
+static inline void init_canary() {
+    STACK_CANARY = 0xDEADBEEF;
+}
 
 // Check for stack overflow
-void check_canary(void);
+static inline void check_canary() {
+    if (STACK_CANARY != 0xDEADBEEF) THROW(SW_STACK_OVERFLOW);
+}
