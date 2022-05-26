88d5ef7835dc444c309e56ab73512887034220be
Ostap Danylovych <19220558+odanylovych@users.noreply.github.com>
2022-05-27 00:39:03 +0300

    base58 encode and decode unit tests

--- unit-tests/test_base58.c
+++ unit-tests/test_base58.c
@@ -9,14 +9,35 @@
 
 #include "common/base58.h"
 
+#define VERIFY_BAD_BASE58_ENCODE(in) \
+    uint8_t out[100] = {0}; \
+    int out_len = 0; \
+    out_len = base58_encode(in, strlen(in), out, sizeof(out)); \
+    assert_int_equal(out_len, -1); \
+    memset(out, 0, sizeof(out));
+
+#define VERIFY_BASE58_ENCODE(in, expected_out) \
+    uint8_t out[100] = {0}; \
+    int out_len = 0; \
+    out_len = base58_encode(in, strlen(in), out, sizeof(out)); \
+    assert_int_equal(out_len, strlen(expected_out)); \
+    assert_string_equal((char *) out, expected_out); \
+    memset(out, 0, sizeof(out));
+
 #define VERIFY_BAD_BASE58_DECODE(in) \
-    out_len = base58_decode(in, sizeof(in) - 1, out, sizeof(out)); \
-    assert_int_equal(out_len, -1);
+    uint8_t out[100] = {0}; \
+    int out_len = 0; \
+    out_len = base58_decode(in, strlen(in), out, sizeof(out)); \
+    assert_int_equal(out_len, -1); \
+    memset(out, 0, sizeof(out));
 
 #define VERIFY_BASE58_DECODE(in, expected_out) \
-    out_len = base58_decode(in, sizeof(in) - 1, out, sizeof(out)); \
+    uint8_t out[100] = {0}; \
+    int out_len = 0; \
+    out_len = base58_decode(in, strlen(in), out, sizeof(out)); \
     assert_int_equal(out_len, strlen(expected_out)); \
-    assert_string_equal((char *) out, expected_out);
+    assert_string_equal((char *) out, expected_out); \
+    memset(out, 0, sizeof(out));
 
 static void test_base58(void **state) {
     (void) state;
@@ -36,23 +57,100 @@ static void test_base58(void **state) {
     assert_string_equal((char *) out2, expected_out2);
 }
 
-static void test_base58_decode(void **state) {
+static void test_bad_base58_encode_out_of_range(void **state) {
     (void) state;
+    VERIFY_BAD_BASE58_ENCODE("1111111111111111111111111111111111111111"
+                             "1111111111111111111111111111111111111111"
+                             "11111111111111111111111111111111111111111");
+}
 
-    uint8_t out[100] = {0};
-    int out_len = 0;
+static void test_base58_encode_zero_characters(void **state) {
+    (void) state;
+    VERIFY_BASE58_ENCODE("", "");
+}
 
+static void test_base58_encode_one_character(void **state) {
+    (void) state;
+    VERIFY_BASE58_ENCODE("!", "a");
+}
+
+static void test_base58_encode_max_number_of_characters(void **state) {
+    (void) state;
+    VERIFY_BASE58_ENCODE("1111111111111111111111111111111111111111"
+                         "1111111111111111111111111111111111111111"
+                         "1111111111111111111111111111111111111111",
+                         "7pURzZxewQxq8JBWyAdgyj3h3xxhjY1cWcyv2GYQ"
+                         "tophYQA4CpLhMJpjKVW8conmkFmRwDd8Ut4XKWiK"
+                         "SbKZ8BqEfXxANAne3kugUNtKHy2S6RKLb9rhCbTq"
+                         "DJFLEwmK7izZWSCbeBU6hph56skH6xcc2DU8B3MWr3Zr");
+}
+
+static void test_bad_base58_decode_zero_characters(void **state) {
+    (void) state;
     VERIFY_BAD_BASE58_DECODE("");
+}
+
+static void test_bad_base58_decode_0_symbol(void **state) {
+    (void) state;
     VERIFY_BAD_BASE58_DECODE("0");
+}
+
+static void test_bad_base58_decode_I_symbol(void **state) {
+    (void) state;
     VERIFY_BAD_BASE58_DECODE("I");
+}
+
+static void test_bad_base58_decode_O_symbol(void **state) {
+    (void) state;
     VERIFY_BAD_BASE58_DECODE("O");
+}
+
+static void test_bad_base58_decode_l_symbol(void **state) {
+    (void) state;
     VERIFY_BAD_BASE58_DECODE("l");
-    VERIFY_BASE58_DECODE("a", "!"); // one character
+}
+
+static void test_bad_base58_decode_plus_symbol(void **state) {
+    (void) state;
+    VERIFY_BAD_BASE58_DECODE("+");
+}
+
+static void test_bad_base58_decode_slash_symbol(void **state) {
+    (void) state;
+    VERIFY_BAD_BASE58_DECODE("/");
+}
+
+static void test_base58_decode_one_character(void **state) {
+    (void) state;
+    VERIFY_BASE58_DECODE("a", "!");
+}
+
+static void test_base58_decode_max_number_of_characters(void **state) {
+    (void) state;
+    VERIFY_BASE58_DECODE("7pURzZxewQxq8JBWyAdgyj3h3xxhjY1cWcyv2GYQ"
+                         "tophYQA4CpLhMJpjKVW8conmkFmRwDd8Ut4XKWiK"
+                         "SbKZ8BqEfXxANAne3kugUNtKHy2S6RKLb9rhCbTq"
+                         "DJFLEwmK7izZWSCbeBU6hph56skH6xcc2DU8B3MWr3Zr",
+                         "1111111111111111111111111111111111111111"
+                         "1111111111111111111111111111111111111111"
+                         "1111111111111111111111111111111111111111");
 }
 
 int main() {
     const struct CMUnitTest tests[] = {cmocka_unit_test(test_base58),
-                                       cmocka_unit_test(test_base58_decode)};
+                                       cmocka_unit_test(test_bad_base58_encode_out_of_range),
+                                       cmocka_unit_test(test_base58_encode_zero_characters),
+                                       cmocka_unit_test(test_base58_encode_one_character),
+                                       cmocka_unit_test(test_base58_encode_max_number_of_characters),
+                                       cmocka_unit_test(test_bad_base58_decode_zero_characters),
+                                       cmocka_unit_test(test_bad_base58_decode_0_symbol),
+                                       cmocka_unit_test(test_bad_base58_decode_I_symbol),
+                                       cmocka_unit_test(test_bad_base58_decode_O_symbol),
+                                       cmocka_unit_test(test_bad_base58_decode_l_symbol),
+                                       cmocka_unit_test(test_bad_base58_decode_plus_symbol),
+                                       cmocka_unit_test(test_bad_base58_decode_slash_symbol),
+                                       cmocka_unit_test(test_base58_decode_one_character),
+                                       cmocka_unit_test(test_base58_decode_max_number_of_characters)};
 
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
